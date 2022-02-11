5586d28cf08762c56a500a70234074e5647cafb1
Brijendrag <90196411+Brijendrag@users.noreply.github.com>
2022-02-11 18:01:37 +0530

    Create Manipulate operation in table.sql

---
+++ Manipulate operation in table.sql
@@ -0,0 +1,35 @@
+TO MANUPULATE THE OPERATIONS ON THE TABLE
+
+
+              UPDATE employees SET last_name = 'Chandra'
+              WHERE employee_id = 101 ;
+
+
+
+           SELECT * FROM TABLE EMPLOYEE;
+
+      | employee_id | first_name  | last_name   | email                             | 
+      +-------------+-------------+-------------+-----------------------------------+
+      |         100 | Brijendra     | goyal     | bhaskar.sahu@sqltutorial.org      | 
+      |         101 | Ram         | Chandra     | ram.mishra@sqltutorial.org        | 
+      |         102 | Jatin       | Chai        | jatin.chai@sqltutorial.org        | 
+      |         103 | Prabhat     | Patel       | prabhat.patel@sqltutorial.org     |
+      |         104 | Rajan       | Baba        | rajan.baba@sqltutorial.org        | 
+      
+    
+
+       DELETE FROM employees WHERE employee_id=103;
+
+      
+       SELECT * FROM TABLE EMPLOYEE;
+
+      
+      
+      | employee_id | first_name  | last_name   | email                             | 
+      +-------------+-------------+-------------+-----------------------------------+
+      |         100 | brijendra      |goyal     | bhaskar.sahu@sqltutorial.org      | 
+      |         101 | Ram       | Chandra     | ram.mishra@sqltutorial.org        | 
+      |         102 | Jatin       | Chai        | jatin.chai@sqltutorial.org        | 
+     
+      |         104 |  Rajan      | Baba        | rajan.baba@sqltutorial.org        | 
+      
